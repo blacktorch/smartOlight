ecb536593888612f6a8f99658def0ec4eec0282f
chidiebere <chidi.onyedinma@fourdrobotics.com>
2020-08-17 23:07:49 -0400

    first commit

---
+++ smart_O_lite.ino
@@ -0,0 +1,54 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 24 // Popular NeoPixel ring size
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetoothLE(10, 11); // RX, TX
+
+int red, green, blue, index;
+
+void setup() {
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+  #endif
+  // END of Trinket-specific code.
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+
+  Serial.begin(9600);
+  bluetoothLE.begin(9600);
+  Serial.println("Started");
+
+}
+
+void loop() {
+  while(bluetoothLE.available() > 0){
+    red = bluetoothLE.parseInt();
+    green = bluetoothLE.parseInt();
+    blue = bluetoothLE.parseInt();
+    index = bluetoothLE.parseInt();
+
+    
+
+    if(bluetoothLE.read() == 'x'){
+      pixels.setPixelColor(index, pixels.Color(red, green, blue));
+      pixels.show();   // Send the updated pixel colors to the hardware.
+      Serial.write(bluetoothLE.read());
+    }
+  }
+
+}
